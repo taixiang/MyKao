d9bc7aa9a3e76fcbf5c35d31cdd94393cb696433
taixiang <taixiang@htyg.com.cn>
2017-08-23 20:09:23 +0800

    27

---
+++ app/src/main/java/com/kaoyan/module/test/TestFragment.java
@@ -0,0 +1,19 @@
+package com.kaoyan.module.test;
+
+import com.kaoyan.base.BaseFragment;
+
+/**
+ * Created by tx on 2017/8/23.
+ */
+
+public class TestFragment extends BaseFragment {
+    @Override
+    protected int attachLayoutRes() {
+        return 0;
+    }
+
+    @Override
+    protected void init() {
+
+    }
+}
